dade99cd805ef4b478a1f8048a2b3333741913eb
Avery Mack <avery.mack@ocsbstudent.ca>
2018-03-22 10:49:50 -0400

    changes

--- main.lua
+++ main.lua
@@ -29,27 +29,53 @@ local randomNumber1
 local randomNumber2
 local userAnswer
 local correctAnswer
+local randomOperator
+local points = 0
+local pointsObject 
 
 
 ---------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
 ---------------------------------------------------------------------------------
 
+-- get rid of the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
 local function AskQuestion()
 	-- generate 2 random numbers between a max. and a min. number 
 	randomNumber1 = math.random (10, 20)
 	randomNumber2 = math.random (10, 20)
+    randomOperator = math.random (1, 3)
 
-	correctAnswer = randomNumber1 + randomNumber2
+    
 
 	-- create question in text object 
-	questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. 	" = " 
+	if randomOperator == 1 then 
+		correctAnswer = randomNumber1 + randomNumber2
+		questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. 	" = " 
+	    
+
+	elseif randomOperator == 2 then 
+		correctAnswer = randomNumber1 - randomNumber2
+		questionObject.text = randomNumber1 .. " - " .. randomNumber2 ..  " = " 
+		
+
+	elseif randomOperator == 3 then
+		correctAnswer = randomNumber1 * randomNumber2
+		questionObject.text = randomNumber1 .. " * " .. randomNumber2 .. " = "
+    end 
 
 end 
 
 local function HideCorrect()
 	correctObject.isVisible = false 
 	incorrectObject.isVisible = false 
+	AskQuestion()
+end 
+
+local function HideIncorrect()
+	incorrectObject.isVisible = false 
+	correctObject.isVisible = false 
 	AskQuestion()
 end 
 
@@ -68,9 +94,11 @@ local function NumericFieldListener(event)
 			-- if the users answer and the correct answer are the same:
 			if (userAnswer == correctAnswer) then
 				correctObject.isVisible = true 
-				timer.performWithDelay(2000, HideCorrect)
+				timer.performWithDelay(1500, HideCorrect)
+				points = points + 1
+				pointsObject.text = points 
 			else incorrectObject.isVisible = true 
-				timer.performWithDelay(20, HideIncorrect)
+				timer.performWithDelay(1500, HideIncorrect)
 			end		
 		end
 	end 
@@ -85,6 +113,11 @@ local function NumericFieldListener(event)
 questionObject = display.newText( "", display.contentWidth/3, display.contentHeight/3, nil, 100) 
 questionObject:setTextColor(0/255, 246/255, 0/255)
 
+-- display a points object and set the colour
+pointsObject = display.newText( "0 ", 900, 700, nil, 100)
+pointsObject:setTextColor(255/255, 255/255, 255/255)
+--pointsObject.text = " 0 "
+
 -- create the correct text object and make it invisible 
 correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
 correctObject:setTextColor(0/255, 246/255, 0/255)
@@ -96,7 +129,7 @@ incorrectObject:setTextColor(225/255, 0/255, 0/255)
 incorrectObject.isVisible = false 
 
 -- create numeric field 
-numericField = native.newTextField( 650, 263, 200, 100)
+numericField = native.newTextField( 675, 263, 200, 100)
 numericField.inputType = "number"
 
 -- add the event listener for the numeric field 
